281d93cddc18d1464b6e4fdd57300e5d7a27f6cf
FITurnip <franklinirtr@gmail.com>
2025-06-27 00:49:15 +0700

    rev

--- routers/tugas_besar.py
+++ routers/tugas_besar.py
@@ -8,13 +8,9 @@ from utils import generate_csv_response, generate_excel_response
 
 router = APIRouter(dependencies=[Depends(get_current_user)])
 
-# -----------------------------
-# Routes
-# -----------------------------
-@router.get("/penilaian/daftar-kelompok")
-async def get_daftar_kelompok(angkatan: int = Query(...)):
+async def getKelompokTubes(matchCondition: dict):
     cursor = db.kelompok_tubes.aggregate([
-        {"$match": {"angkatan": angkatan}},
+        {"$match": matchCondition},
         {
             "$lookup": {
                 "from": "users",
@@ -38,10 +34,19 @@ async def get_daftar_kelompok(angkatan: int = Query(...)):
     doc = await cursor.to_list(length=None)
     return convert_objectid(doc)
 
-@router.get("/penilaian/aspek-penilaian-kelompok")
-async def get_aspek_penilaian_kelompok(tahun: int = Query(...)):
+async def getNilaiKelompokTubes(id_kelompok: str):
+    doc = await db.nilai_kelompok.find_one({"id_kelompok": ObjectId(id_kelompok)})
+    if not doc:
+        return []
+    
+    return doc
+
+async def getAspekPenilaianKelompok(tahun: List[int]):
     cursor = db.aspek_penilaian_kelompok.aggregate([
-        {"$match": {"isParent": True, "tahun": tahun}},
+        {"$match": {
+            "isParent": True,
+            "tahun": { "$in" : tahun }
+        }},
         {"$lookup": {
             "from": "aspek_penilaian_kelompok",             # collection yang sama
             "localField": "_id",            # parent _id
@@ -53,13 +58,60 @@ async def get_aspek_penilaian_kelompok(tahun: int = Query(...)):
     doc = await cursor.to_list(length=None)
     return convert_objectid(doc)
 
+async def getAspekPenilaianPerorangan(tahun: int):
+    cursor = db.aspek_penilaian_perorangan.aggregate([
+        {"$match": {
+            "isParent": True,
+            "tahun": { "$in" : tahun }
+        }},
+        {"$lookup": {
+            "from": "aspek_penilaian_perorangan",             # collection yang sama
+            "localField": "_id",            # parent _id
+            "foreignField": "parentId",    # child parentId
+            "as": "children"
+        }}
+    ])
+
+    doc = await cursor.to_list(length=None)
+    return convert_objectid(doc)
+
+def extract_children_only(data, key_children="children"):
+    result = []
+
+    def recurse(node, has_parent=False):
+        if has_parent:
+            flat_node = {k: v for k, v in node.items() if k != key_children}
+            result.append(flat_node)
+        for child in node.get(key_children, []):
+            recurse(child, has_parent=True)
+
+    if isinstance(data, list):
+        for item in data:
+            recurse(item, has_parent=False)
+    elif isinstance(data, dict):
+        recurse(data, has_parent=False)
+    else:
+        raise TypeError("Input harus berupa dict atau list of dicts")
+
+    return result
+
+# -----------------------------
+# Routes
+# -----------------------------
+@router.get("/penilaian/daftar-kelompok")
+async def get_daftar_kelompok(angkatan: int = Query(...)):
+    daftarKelompok = await getKelompokTubes({"angkatan": angkatan})
+    return daftarKelompok
+
+@router.get("/penilaian/aspek-penilaian-kelompok")
+async def get_aspek_penilaian_kelompok(tahun: int = Query(...)):
+    doc = await getAspekPenilaianKelompok([tahun])
+    return doc
+
 @router.get("/penilaian/nilai-kelompok")
 async def get_nilai_kelompok(id_kelompok: str = Query(...)):
-    doc = await db.nilai_kelompok.find_one({"id_kelompok": ObjectId(id_kelompok)})
-    if not doc:
-        return []
-    
-    return convert_objectid(doc)
+    doc = await getNilaiKelompokTubes(id_kelompok)
+    return doc
 
 @router.post("/penilaian/nilai-kelompok")
 async def post_nilai_kelompok(request: Request):
@@ -86,45 +138,13 @@ async def post_nilai_kelompok(request: Request):
 
 @router.get("/penilaian/aspek-penilaian-perorangan")
 async def get_aspek_penilaian_perorangan(tahun: int = Query(...)):
-    cursor = db.aspek_penilaian_perorangan.aggregate([
-        {"$match": {"isParent": True, "tahun": tahun}},
-        {"$lookup": {
-            "from": "aspek_penilaian_perorangan",             # collection yang sama
-            "localField": "_id",            # parent _id
-            "foreignField": "parentId",    # child parentId
-            "as": "children"
-        }}
-    ])
-
-    doc = await cursor.to_list(length=None)
-    return convert_objectid(doc)
+    doc = await getAspekPenilaianPerorangan([tahun])
+    return doc
 
 @router.get("/penilaian/nilai-perorangan")
 async def get_nilai_perorangan(id_kelompok: str = Query(...)):
     # Ambil info kelompok & anggota
-    cursor_kelompok_tubes = db.kelompok_tubes.aggregate([
-        {"$match": {"_id": ObjectId(id_kelompok)}},
-        {
-            "$lookup": {
-                "from": "users",
-                "localField": "nim_anggota",
-                "foreignField": "nim",
-                "as": "anggota"
-            }
-        },
-        {
-            "$project": {
-                "nomor": 1,
-                "kelas": 1,
-                "angkatan": 1,
-                "laporan": 1,
-                "anggota.nama": 1,
-                "anggota.nim": 1,
-            }
-        }
-    ])
-
-    doc_kelompok_tubes = await cursor_kelompok_tubes.to_list(length=None)
+    doc_kelompok_tubes = await getKelompokTubes({"_id": ObjectId(id_kelompok)})
     if not doc_kelompok_tubes:
         raise HTTPException(status_code=404, detail="Kelompok tidak ditemukan")
 
@@ -171,36 +191,104 @@ async def post_nilai_perorangan(request: Request):
 
     return convert_objectid(upserted_ids)
 
+async def getNilaiPerKelompok(tahun, kelas):
+# dapatkan data kelompok tubes dan aspek penilaian kelompok
+    kelompok_tubes_condition = {
+        "angkatan": { "$in" : tahun },
+        "kelas": { "$in" : kelas }
+    }
+    doc_kelompok_tubes = await getKelompokTubes(kelompok_tubes_condition)
+    doc_aspek_penilaian_kelompok = await getAspekPenilaianKelompok(tahun)
+    doc_aspek_penilaian_kelompok = extract_children_only(doc_aspek_penilaian_kelompok)
+
+    for kelompok in doc_kelompok_tubes:
+        # ambil nilai kelompok
+        doc_nilai_kelompok = await db.nilai_kelompok.find_one({"id_kelompok": ObjectId(kelompok["id"])})
+        
+        nilaiAkhir = 0
+        if doc_nilai_kelompok:
+            item_nilai_kelompok = convert_objectid(doc_nilai_kelompok.get("nilai", []))
+            for item in item_nilai_kelompok:
+                # cari bobot dari aspek penilaian
+                bobot = next((ap["bobot"] for ap in doc_aspek_penilaian_kelompok if ap["id"] == item["aspek_penilaian_id"]), 0)
+                # jumlahkan nilai akhir
+                nilaiAkhir += (item["nilai"] * bobot / 100)
+        
+        kelompok["nilaiAkhir"] = nilaiAkhir
+    return doc_kelompok_tubes
+
 @router.get("/rekap/nilai-kelompok")
 async def get_rekap_nilai_kelompok(
-    tahun: int = Query(...), 
-    kelas: str = Query(...),
+    tahun: List[int] = Query(...), 
+    kelas: List[str] = Query(...),
     format: str = Query("json")
 ):
-    query = {"tahun": tahun, "kelas": kelas}
-    cursor = db.nilai_kelompok.find(query)
-    data = [convert_objectid(doc) async for doc in cursor]
+    doc_kelompok_tubes = await getNilaiPerKelompok(tahun, kelas)
+    doc_kelompok_tubes = sorted(doc_kelompok_tubes, key=lambda x: x["nilaiAkhir"], reverse=True)
 
     if format == "csv":
-        return generate_csv_response(data, filename="rekap_nilai.csv")
+        return generate_csv_response(doc_kelompok_tubes, filename="rekap_nilai.csv")
     elif format == "excel":
-        return generate_excel_response(data, filename="rekap_nilai.xlsx", sheet_name="Nilai")
+        return generate_excel_response(doc_kelompok_tubes, filename="rekap_nilai.xlsx", sheet_name="Nilai")
     else:
-        return data
+        return doc_kelompok_tubes
 
 @router.get("/rekap/nilai-perorangan")
 async def get_rekap_nilai_perorangan(
-    tahun: int = Query(...), 
-    kelas: str = Query(...),
+    tahun: List[int] = Query(...), 
+    kelas: List[str] = Query(...),
     format: str = Query("json")
 ):
-    query = {"tahun": tahun, "kelas": kelas}
-    cursor = db.nilai_perorangan.find(query)
-    data = [convert_objectid(doc) async for doc in cursor]
+    doc_kelompok_tubes = await getNilaiPerKelompok(tahun, kelas)
+    doc_aspek_penilaian_perorangan = await getAspekPenilaianPerorangan(tahun)
+    doc_aspek_penilaian_perorangan = extract_children_only(doc_aspek_penilaian_perorangan)
+
+    doc_rekap_nilai_perorangan = []
+    for kelompok in doc_kelompok_tubes:
+        anggota = kelompok.get("anggota", [])
+        for anggota_item in anggota:
+            # ambil nilai perorangan
+            doc_nilai_perorangan = await db.nilai_perorangan.find_one({"nim": anggota_item["nim"]})
+            nilai_kelompok = kelompok.get("nilaiAkhir", 0)
+            if not doc_nilai_perorangan:
+                nilaiAkhir = (nilai_perorangan + nilai_kelompok) / 2
+                doc_rekap_nilai_perorangan.append({
+                    "nama": anggota_item["nama"],
+                    "nim": anggota_item["nim"],
+                    "kelas": kelompok["kelas"],
+                    "angkatan": kelompok["angkatan"],
+                    "nomor": kelompok["nomor"],
+                    "nilaiPerorangan": 0,
+                    "nilaiKelompok": nilai_kelompok,
+                    "nilaiAkhir": nilaiAkhir
+                })
+                continue
+            
+            # konversi nilai perorangan
+            data_nilai_perorangan = convert_objectid(doc_nilai_perorangan.get("nilai", []))
+            nilai_perorangan = 0
+            for item in data_nilai_perorangan:
+                # cari bobot dari aspek penilaian
+                bobot = next((ap["bobot"] for ap in doc_aspek_penilaian_perorangan if ap["id"] == item["aspek_penilaian_id"]), 0)
+                # jumlahkan nilai akhir
+                nilai_perorangan += (item["nilai"] * bobot / 100)
+
+            nilaiAkhir = (nilai_perorangan + nilai_kelompok) / 2
+            doc_rekap_nilai_perorangan.append({
+                "nama": anggota_item["nama"],
+                "nim": anggota_item["nim"],
+                "kelas": kelompok["kelas"],
+                "angkatan": kelompok["angkatan"],
+                "nomor": kelompok["nomor"],
+                "nilaiPerorangan": nilai_perorangan,
+                "nilaiKelompok": nilai_kelompok,
+                "nilaiAkhir": nilaiAkhir
+            })
+    doc_rekap_nilai_perorangan = sorted(doc_rekap_nilai_perorangan, key=lambda x: x["nilaiAkhir"], reverse=True)
 
     if format == "csv":
-        return generate_csv_response(data, filename="rekap_nilai.csv")
+        return generate_csv_response(doc_rekap_nilai_perorangan, filename="rekap_nilai.csv")
     elif format == "excel":
-        return generate_excel_response(data, filename="rekap_nilai.xlsx", sheet_name="Nilai")
+        return generate_excel_response(doc_rekap_nilai_perorangan, filename="rekap_nilai.xlsx", sheet_name="Nilai")
     else:
-        return data
+        return doc_rekap_nilai_perorangan
--- utils.py
+++ utils.py
@@ -3,29 +3,37 @@ import io
 import pandas as pd
 from fastapi.responses import StreamingResponse
 
-def generate_csv_response(data: list[dict], filename: str = "data.csv") -> StreamingResponse:
-    buffer = io.StringIO()
-    writer = csv.DictWriter(buffer, fieldnames=data[0].keys())
+def generate_csv_response(data, filename="data.csv"):
+    output = io.StringIO()
+    writer = csv.DictWriter(output, fieldnames=data[0].keys())
     writer.writeheader()
     writer.writerows(data)
-    buffer.seek(0)
+
+    headers = {
+        "Content-Disposition": f'attachment; filename="{filename}"',
+        "Access-Control-Expose-Headers": "Content-Disposition",
+    }
 
     return StreamingResponse(
-        iter([buffer.getvalue()]),
+        iter([output.getvalue()]),
         media_type="text/csv",
-        headers={"Content-Disposition": f"attachment; filename={filename}"}
+        headers=headers
     )
 
-
-def generate_excel_response(data: list[dict], filename: str = "data.xlsx", sheet_name: str = "Sheet1") -> StreamingResponse:
+def generate_excel_response(data, filename="data.xlsx", sheet_name="Sheet1"):
     df = pd.DataFrame(data)
-    buffer = io.BytesIO()
-    with pd.ExcelWriter(buffer, engine="openpyxl") as writer:
-        df.to_excel(writer, index=False, sheet_name=sheet_name)
-    buffer.seek(0)
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
+    output.seek(0)
+
+    headers = {
+        "Content-Disposition": f'attachment; filename="{filename}"',
+        "Access-Control-Expose-Headers": "Content-Disposition",
+    }
 
     return StreamingResponse(
-        buffer,
+        output,
         media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        headers={"Content-Disposition": f"attachment; filename={filename}"}
+        headers=headers
     )
